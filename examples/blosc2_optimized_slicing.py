c52bdb67eb04fc3db0a2da9402160c495fb510f7
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2023-11-29 12:24:57 +0100

    Remove copyright notice from optimized slicing example file.
    It was created from scratch by Blosc developers anyway.

--- examples/blosc2_optimized_slicing.py
+++ examples/blosc2_optimized_slicing.py
@@ -1,12 +1,3 @@
-# This file is part of h5py, a Python interface to the HDF5 library.
-#
-# http://www.h5py.org
-#
-# Copyright 2008-2023 Andrew Collette and contributors
-#
-# License:  Standard 3-clause BSD; see "license.txt" for full license terms
-#           and contributor agreement.
-
 """Example of Blosc2 NDim optimized slicing.
 
 It creates a 2-dimensional dataset made of different chunks, compressed with
