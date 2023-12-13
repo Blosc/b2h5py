71a7bd28941060b78215a5e5463b274d219026e0
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2023-12-13 09:10:55 +0100

    Add module for automatic, global enabling of optimizations.
    This provides a very simple interface to enabling the optimizations, while
    allowing the plain import to only perform the dataset class patching
    explicitly.

---
+++ b2h5py/auto.py
@@ -0,0 +1,14 @@
+"""Automatic activation of Blosc2 optimized slicing for h5py.
+
+Importing this module enables the optimization globally, just use::
+
+    import b2h5py.auto
+
+After that, all slicing operations on Blosc2-compressed datasets will be
+transparently optimized when possible.
+"""
+
+from .blosc2 import enable_fast_slicing
+
+
+enable_fast_slicing()
