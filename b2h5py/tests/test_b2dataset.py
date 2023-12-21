1374e0a8d2bf69e47a25ad3f9d3d3780aa7457c5
Thomas VINCENT <thomas.vincent@esrf.fr>
2023-12-21 13:57:24 +0100

    convert test to unittest

--- b2h5py/tests/test_b2dataset.py
+++ b2h5py/tests/test_b2dataset.py
@@ -1,37 +1,47 @@
 """Test using B2Dataset to enable fast access to Blosc2 compressed dataset.
 """
 
+import os.path
+import tempfile
+from unittest import TestCase
+
 import h5py
 import hdf5plugin
 import numpy as np
-import pytest
 
 from b2h5py import B2Dataset
 
 
-@pytest.fixture
-def hdf5_test_dataset(tmp_path):
-    """Text fixture creating a hdf5 file with a "data" dataset compression with Blosc2"""
-    shape = 3500, 300
-    chunks = 1747, 150
-    data = np.arange(np.prod(shape), dtype="u2").reshape(shape)
+class TestB2Dataset(TestCase):
+    def setUp(self):
+        shape = 3500, 300
+        chunks = 1747, 150
+        self.data = np.arange(np.prod(shape), dtype="u2").reshape(shape)
 
-    with h5py.File(tmp_path / "test_file.h5", "w") as f:
-        compression = hdf5plugin.Blosc2(cname='lz4', clevel=5, filters=hdf5plugin.Blosc2.SHUFFLE)
-        f.create_dataset('data', data=data, chunks=chunks, **compression)
+        self.tempdir = tempfile.TemporaryDirectory()
+        filename = os.path.join(self.tempdir.name, "test_file.h5")
 
-    with h5py.File(tmp_path / "test_file.h5", "r") as f:
-        yield f["data"]
+        with h5py.File(filename, "w") as f:
+            compression = hdf5plugin.Blosc2(cname='lz4', clevel=5, filters=hdf5plugin.Blosc2.SHUFFLE)
+            f.create_dataset('data', data=self.data, chunks=chunks, **compression)
 
+        self.h5file = h5py.File(filename, "r")
+        self.h5dataset = self.h5file["data"]
 
-def test_B2Dataset(hdf5_test_dataset):
-    b2dataset = B2Dataset(hdf5_test_dataset)
-    assert b2dataset.is_b2_fast_access
+    def tearDown(self):
+        self.h5dataset = None
+        self.h5file.close()
+        self.tempdir.cleanup()
 
-    # Access h5py.Dataset attribute
-    assert b2dataset.chunks == hdf5_test_dataset.chunks
+    def testB2Dataset(self):
+        b2dataset = B2Dataset(self.h5dataset)
+        self.assertTrue(b2dataset.is_b2_fast_access)
 
-    # Access whole array
-    assert np.array_equal(b2dataset[()], hdf5_test_dataset[()])
-    # Unoptimized access
-    assert np.array_equal(b2dataset[::2], hdf5_test_dataset[::2])
+        # Access h5py.Dataset attribute
+        self.assertEqual(b2dataset.chunks, self.h5dataset.chunks)
+
+        # Access whole array
+        self.assertTrue(np.array_equal(b2dataset[()], self.data))
+        # Unoptimized access
+        self.assertTrue(np.array_equal(b2dataset[::2], self.data[::2]))
+
