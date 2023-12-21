10e56623ee1fef4a92a5c54e0dc81a5e5edb8bda
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2023-12-21 17:31:50 +0100

    Factor out common code for test storage and optimization checking.
    Reuse in `B2Dataset` tests too.

--- b2h5py/tests/test_b2dataset.py
+++ b2h5py/tests/test_b2dataset.py
@@ -1,43 +1,30 @@
 """Test using B2Dataset to enable fast access to Blosc2 compressed dataset.
 """
 
-import h5py
-import hdf5plugin
 import numpy as np
 
 from b2h5py import B2Dataset
+from b2h5py.tests.common import StoreArrayMixin
 from h5py.tests.common import TestCase
 
 
-class TestB2Dataset(TestCase):
+class TestB2Dataset(TestCase, StoreArrayMixin):
     def setUp(self):
-        super().setUp()
+        TestCase.setUp(self)
 
         shape = 3500, 300
-        chunks = 1747, 150
-        self.data = np.arange(np.prod(shape), dtype="u2").reshape(shape)
-
-        compression = hdf5plugin.Blosc2(cname='lz4', clevel=5, filters=hdf5plugin.Blosc2.SHUFFLE)
-        self.f.create_dataset('data', data=self.data, chunks=chunks, **compression)
-
-        filename = self.f.filename
-        self.f.close()
-        self.f = h5py.File(filename, "r")
-        self.h5dataset = self.f["data"]
-
-    def tearDown(self):
-        self.h5dataset = None
-        super().tearDown()
+        self.chunks = 1747, 150
+        self.arr = np.arange(np.prod(shape), dtype="u2").reshape(shape)
+        StoreArrayMixin.setUp(self)
 
     def testB2Dataset(self):
-        b2dataset = B2Dataset(self.h5dataset)
+        b2dataset = B2Dataset(self.dset)
         self.assertTrue(b2dataset.is_b2_fast_access)
 
         # Access h5py.Dataset attribute
-        self.assertEqual(b2dataset.chunks, self.h5dataset.chunks)
+        self.assertEqual(b2dataset.chunks, self.dset.chunks)
 
         # Access whole array
-        self.assertArrayEqual(b2dataset[()], self.data)
+        self.assertArrayEqual(b2dataset[()], self.arr)
         # Unoptimized access
-        self.assertArrayEqual(b2dataset[::2], self.data[::2])
-
+        self.assertArrayEqual(b2dataset[::2], self.arr[::2])
--- b2h5py/tests/test_slicing_blosc2.py
+++ b2h5py/tests/test_slicing_blosc2.py
@@ -5,54 +5,15 @@ be used.
 """
 
 import os
-import functools
 import random
 
 import b2h5py
-import hdf5plugin as h5p
 import numpy as np
 
-from h5py import File
+from b2h5py.tests.common import (Blosc2OptNotUsedError,
+                                 StoreArrayMixin,
+                                 check_opt_slicing)
 from h5py.tests.common import TestCase
-
-
-class Blosc2OptNotUsedError(Exception):
-    """Blosc2 optimization was not used by unit test"""
-    pass
-
-
-class StoreArrayMixin:
-    # Requires: self.f (read/write), self.arr, self.chunks
-    # Provides: self.f (read-only), self.dset
-    def setUp(self):
-        comp = h5p.Blosc2(cname='lz4', clevel=5, filters=h5p.Blosc2.SHUFFLE)
-        self.f.create_dataset('x', data=self.arr, chunks=self.chunks, **comp)
-
-        # Reopen the test file read-only to ensure
-        # that no HDF5/h5py caching takes place.
-        fn = self.f.filename
-        self.f.close()
-        self.f = File(fn, 'r')
-        self.dset = self.f['x']
-
-
-def check_opt_slicing(test):
-    """Decorate `test` to fail if slicing did not use expected optimization"""
-    @functools.wraps(test)
-    def checked_test(self):
-        if not self.should_enable_opt():
-            return test(self)
-        # If the dataset class is not patched,
-        # the exception set below is never raised anyway.
-        self.assertTrue(b2h5py.is_fast_slicing_enabled())
-        # Force an exception if the optimization is not used.
-        orig_exc = b2h5py.blosc2._no_opt_error
-        b2h5py.blosc2._no_opt_error = Blosc2OptNotUsedError
-        try:
-            return test(self)
-        finally:
-            b2h5py.blosc2._no_opt_error = orig_exc
-    return checked_test
 
 
 class Blosc2OptSlicingTestCase(TestCase, StoreArrayMixin):
